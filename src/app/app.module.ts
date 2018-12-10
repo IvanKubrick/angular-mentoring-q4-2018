edb5c7d26ee82a878ffee96148d8dd43e6f7707c
Ivan Grushevich <githubmail911@gmail.com>
2018-12-11 00:15:41 +0300

    Move header component to course module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { CoreModule } from '@app/core';
 import { CoursesModule } from './courses/courses.module';
@@ -8,13 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './page/header/header.component';
 import { FooterComponent } from './page/footer/footer.component';
-import { SearchComponent } from './page/search/search.component';
 import { BreadcrumbsComponent } from './page/header/breadcrumbs/breadcrumbs.component';
 import { LogoComponent } from './page/logo/logo.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, SearchComponent, BreadcrumbsComponent, LogoComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, CoursesModule, FormsModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, BreadcrumbsComponent, LogoComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, CoursesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,11 +1,13 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SearchComponent } from './search/search.component';
 import { CourseListComponent } from './course-list/course-list.component';
 import { CourseItemComponent } from './course-list/course-item/course-item.component';
 
 @NgModule({
-  declarations: [CourseListComponent, CourseItemComponent],
-  imports: [CommonModule],
-  exports: [CourseListComponent, CourseItemComponent]
+  declarations: [CourseListComponent, CourseItemComponent, SearchComponent],
+  imports: [CommonModule, FormsModule]
 })
 export class CoursesModule {}
