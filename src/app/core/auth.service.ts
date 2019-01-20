9203c8a6728ba7201e042d1481129dfb8204bb37
Ivan Grushevich <githubmail911@gmail.com>
2019-01-21 02:57:27 +0300

    Update Auth service

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
-import { IAuthData, IUser } from '@app/shared';
+import { IAuthData } from '@app/shared';
+
+const lsKey: string = 'ngCourses';
 
 // tslint:disable-next-line: no-unsafe-any
 @Injectable()
@@ -10,18 +13,29 @@ export class AuthService {
   private _isAuthenticated: boolean;
 
   get isAuthenticated(): boolean {
-    return false;
+    return this._isAuthenticated;
   }
 
+  constructor(private readonly router: Router) {}
+
   login(authData: IAuthData): void {
+    this.userData = authData;
+    localStorage.setItem(lsKey, JSON.stringify(authData));
     this._isAuthenticated = true;
+    window.console.log('logged in successfully');
+
+    this.router.navigate(['/courses']);
   }
 
   logout(): void {
-    window.console.log('logout');
+    localStorage.removeItem(lsKey);
     this.userData = null;
     this._isAuthenticated = false;
+    window.console.log('logout');
+
+    this.router.navigate(['/']);
   }
+
   getUserInfo(): IAuthData {
     return this.userData;
   }
